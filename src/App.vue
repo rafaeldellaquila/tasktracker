77ec656064f1c1ad8b4d08decf95c74e02d247c6
Rafael Dell'Aquila <rafael@dellaquila.dev>
2022-04-04 20:12:01 -0300

    :sparkles: start the project and :lipstick: installing Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <main class="columns is-gapless is-multiline">
+        <div class="column is-one-quarter">
+            <side-bar />
+        </div>
+
+        <div class="column is-three-quarter">
+            <form-task />
+        </div>
+    </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue"
+import SideBar from "@/components/SideBar.vue"
+import FormTask from "@/components/FormTask.vue"
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+    name: "App",
+    components: {
+        SideBar,
+        FormTask,
+    },
+})
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
 </style>
