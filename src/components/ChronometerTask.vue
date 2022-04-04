2813ebe2e09303f44cefe84393173fc126d8b4f5
Rafael Dell'Aquila <rafael@dellaquila.dev>
2022-04-04 20:14:19 -0300

    :sparkles: start the timer components :lipstick: and stylize then

---
+++ src/components/ChronometerTask.vue
@@ -0,0 +1,29 @@
+<template>
+    <section>
+        <strong>
+            <i class="ri-timer-line" />
+            {{ elapsedTime }}
+        </strong>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+export default defineComponent({
+    name: "ChronometerTask",
+
+    props: {
+        timeInSeconds: {
+            type: Number,
+            default: 0,
+        },
+    },
+    computed: {
+        elapsedTime(): string {
+            return new Date(this.timeInSeconds * 1000).toISOString().substring(11, 19)
+        },
+    },
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/TimerTask.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="is-flex is-align-items-center is-justify-content-space-between">
+        <chronometer-task :timeInSeconds="timeInSeconds" />
+        <div class="is-flex is-align-items-center">
+            <button class="button" @click="initCount" :disabled="timerOn">
+                <i class="icon ri-play-line" />
+                <span> play </span>
+            </button>
+
+            <button class="button ml-4" @click="stopCount" :disabled="!timerOn">
+                <i class="icon ri-stop-fill" />
+                <span> stop </span>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import ChronometerTask from "./ChronometerTask.vue"
+export default defineComponent({
+    name: "TimerTask",
+
+    components: { ChronometerTask },
+
+    data() {
+        return {
+            timeInSeconds: 0,
+            chronometer: 0,
+            timerOn: false,
+        }
+    },
+
+    methods: {
+        initCount() {
+            this.timerOn = true
+            this.chronometer = setInterval(() => {
+                this.timeInSeconds += 1
+            }, 1000)
+        },
+        stopCount() {
+            this.timerOn = false
+            clearInterval(this.chronometer)
+        },
+    },
+})
+</script>
+
+<style lang="scss" scoped></style>
