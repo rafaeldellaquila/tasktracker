e1f5011d04e03b6bfae8a6f30742f3fa5a833500
Rafael Dell'Aquila <rafael@dellaquila.dev>
2022-04-04 20:13:50 -0300

    :sparkles: start the main components SideBar and Form

---
+++ src/components/FormTask.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" name="task-input" id="task-input" class="input" placeholder="Qual tarefa você deseja iniciar?" />
+            </div>
+
+            <div class="column">
+                <timer-task />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import TimerTask from "./TimerTask.vue"
+export default defineComponent({
+    name: "FormTask",
+
+    components: { TimerTask },
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="is-flex is-justify-content-center p-4">
+        <h1 class="mt-6"><img src="@/assets/logo.svg" alt="tasktracker logo"/></h1>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'SideBar',
+});
+</script>
+
+<style lang="scss" scoped>
+header {
+    background: #414141;
+    width: 100%;
+    height: 100vh;
+
+    @media only screen and (max-width: 768px) {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+
+
+</style>
